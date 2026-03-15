dc9b1e85e27d06c1a5f7a3afeed1c0bfc80dc008
akmoex <akmoex@gmail.com>
2026-03-15 07:32:40 +0000

    nq数据集0.77

---
+++ my_scripts/nq_new_feature.sh
@@ -0,0 +1,37 @@
+## 微调检索器, 其实不需要动, 用之前调好的就行
+cd retrieval
+python finetune_retriever.py \
+    --dataset_name nq_10 \
+    --input_path 10_total_documents/nq-open-10_total_documents_gold_at_0.jsonl.gz \
+    --model_name google-bert/bert-base-uncased \
+    --train_batch_size 32 \
+    --num_epoch 4 \
+    --model_save_path ../output/mine/retrievers/nq10_bert
+
+
+## 提取特征
+cd retrieval
+HF_ENDPOINT=https://hf-mirror.com python feature_extraction.py \
+    --dataset_name nq_10 \
+    --input_path 10_total_documents/nq-open-10_total_documents_gold_at_0.jsonl.gz \
+    --save_path ../output/mine/features/nq-open-10_total_documents_gold_at_0_bert.pkl \
+    --model_name google-bert/bert-base-uncased \
+    --model_name ../output/rrag/retrievers/nq10_bert
+
+## 训练和评估
+nohup python runner.py \
+  --cuda_visible_devices 0,1 \
+  --dataset_name nq_10 \
+  --input_path output/mine/features/nq-open-10_total_documents_gold_at_0_bert.pkl \
+  --model_name /home/akmoex/.cache/modelscope/hub/models/shakechen/Llama-2-7b-hf \
+  --load_in_8bit \
+  --use_training \
+  --save_model \
+  --output_dir output/mine/llama-2-7b \
+  --freeze_llm \
+  --num_k 10 \
+  --use_rrag \
+  --use_evaluation \
+  --save_results \
+  > output/mine/llama-2-7b/train.log 2>&1 &
+
